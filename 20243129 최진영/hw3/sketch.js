43ce00b87dcc211fcab752bb84a1afb4d1f938be
jinyeongchoi487-collab <jinyeongchoi487@gmail.com>
2025-11-20 03:06:33 +0900

    Add files via upload

---
+++ 20243129 최진영/hw3/sketch.js
@@ -0,0 +1,16 @@
+let img;
+
+function preload() {
+  img = loadImage("20243129_최진영_과제3-1.gif");
+}
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(220);
+  image(img, 0, 0, width, height);
+}
+
+
